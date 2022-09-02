be7e3854901c2463709daa7957f468f0dd7101ba
Aidan Ceney <aidanceney@gmail.com>
2022-09-02 16:35:03 -0600

    update to sting_matrix gixed bugs

--- src/string_matrix.jl
+++ src/string_matrix.jl
@@ -10,13 +10,21 @@ function make_string_matrix(df, properties_matrix)
 end
 
 function init_string_matrix(nrow_string_matrix,ncol_string_matrix)
-	string_matrix = Matrix{String}
+	string_matrix = ""
 	for i in 1:nrow_string_matrix
-		tmp_vec = Vector{String}
+		tmp_vec = ""
 		for j in 1:ncol_string_matrix
-			tmp_vec = [tmp_vec ""]
+			if j == 1
+				tmp_vec = [""]
+			else
+				tmp_vec = [tmp_vec ""]
+			end
 		end
-		string_matrix = [string_matrix;tmp_vec]
+		if i == 1 
+			string_matrix = tmp_vec
+		else
+			string_matrix = [string_matrix;tmp_vec]
+		end
 	end
 	return string_matrix
 end
@@ -24,7 +32,7 @@ end
 function write_properties(string_matrix,properties_cell,df_cell,i,j)
 	for (property_func, property_used) in properties_cell
 		    if property_used
-			getfield(Main, Symbol(properties_cell))(string_matrix,df_cell,i,j)
+			getfield(Main, Symbol(property_func))(string_matrix,df_cell,i,j)
 		    end
 	end
 end
