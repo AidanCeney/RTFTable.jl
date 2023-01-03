f39946b9a788f6f7587bef5f990f5d89bfd117af
Aidan Ceney <aidanceney@gmail.com>
2023-01-02 17:32:32 -0700

    added test for writtinng to value

--- test/test-settings.jl
+++ test/test-settings.jl
@@ -1,5 +1,5 @@
 
-@testset "settings" begin
+@testset "settings_onoff" begin
     jtable.set_property_default("bold",true,"")
     df = DataFrame(A=1:4,B = ["M", "F", "F", "M"])
     dt = jtable.make_data_table(df)
@@ -11,5 +11,21 @@
 	aTest = jtable.getAll(dt.property_matrix,"bold")
     @test all(i -> i == false,aTest)
 end;
+
+@testset "settings_value" begin
+    jtable.set_property_default("fs",true,"22")
+    df = DataFrame(A=1:4,B = ["M", "F", "F", "M"])
+    dt = jtable.make_data_table(df)
+	aTest = jtable.getAll(dt.value_matrix,"fs")
+    @test all(i -> i == "22", aTest)
+    jtable.delete_property_default("fs")
+    df = DataFrame(A=1:4,B = ["M", "F", "F", "M"])
+    dt = jtable.make_data_table(df)
+	aTest = jtable.getAll(dt.value_matrix,"fs")
+    @test all(i -> i == "20", aTest)
+end;
+
+
+
 
 
